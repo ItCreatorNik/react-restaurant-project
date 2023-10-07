40c47cd161caa06ca51c3c8297d764f4e5fb6d44
Nikita Kovalyov <boba.bibap@gmail.com>
2023-10-07 17:17:12 +0300

    hw2-fixed

--- react-restaurant/src/pages/Main/component.jsx
+++ react-restaurant/src/pages/Main/component.jsx
@@ -1,24 +1,18 @@
 import { Tabs } from "../../components/Tabs/component";
 import { restaurants } from "../../constants/mock";
-import { Menu } from "../../components/Menu/component";
-import { Reviews } from "../../components/Reviews/component";
 import {useState} from "react";
+import { Restaurant } from '../../components/Restaurant/component';
 
 const tabs = restaurants.map(restaurant => ({ id: restaurant.id, text: restaurant.name }))
 
 export const Main = () => {
-    const [restaurantId, setRestaurantId] = useState(restaurants[0].id)
+    const [activeRestaurantIndex, setActiveRestaurantIndex] = useState(0)
 
-    const activeRestaurant = restaurants.find(restaurant => restaurant.id === restaurantId)
-    const setActiveRestaurant = (id) => {
-        setRestaurantId(id)
-    }
+  
     return (
-        <div>
-            <Tabs tabs={tabs} onChange={setActiveRestaurant}/>
-            <h2>{activeRestaurant.name}</h2>
-            <Menu menu={activeRestaurant.menu}/>
-            <Reviews reviews={activeRestaurant.reviews}/>
-        </div>
-    )
+      <div>
+        <Tabs tabs={tabs} onTabSelect={setActiveRestaurantIndex} />
+        <Restaurant restaurant={restaurants[activeRestaurantIndex]} />
+      </div>
+    );
 }
